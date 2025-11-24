b694e27572a33bf986643729ad4b7ded1e67971f
LindaPermata <permatalinda531@gmail.com>
2025-11-24 09:09:07 +0700

    membuat file db.js untuk konfigurasi database di folder config

--- models/komik.js
+++ models/komik.js
@@ -1,31 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
   const Komik = sequelize.define('Komik', {
-    id: {
-        type: DataTypes.INTEGER,
-        autiIncrement: true,
-        primaryKey: true
-    },
-    title: {
-        type: DataTypes.STRING,
+            id: {
+                type: DataTypes.INTEGER,
+                autiIncrement: true,
+                primaryKey: true
+            },
+            title: {
+                type: DataTypes.STRING,
 
-    },
-    description: {
-        type: DataTypes.STRING,
-    },
-    author: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    imageType: DataTyoes.STRING,
-    imageName: DataTypes.STRING,
-    imageData: DataTypes.BLOB('long'),
+            },
+            description: {
+                type: DataTypes.STRING,
+            },
+            author: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            imageType: DataTyoes.STRING,
+            imageName: DataTypes.STRING,
+            imageData: DataTypes.BLOB('long'),
 
-    }, 
-    
-    {
-    tableName: 'komik',
-    }
-);
+        },    
+        {
+        tableName: 'komik',
+        }
+    );
+
     return Komik;
-}
+};
 
